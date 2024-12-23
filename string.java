abda8e554b8a3453d47fbad8d8455a5a83ec4d99
MD PARVEJ <mparvejind@gmail.com>
2024-12-23 23:23:20 +0530

    upd

---
+++ string.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class string {
+    public static void main(String[] args) {
+        // Strings
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Your name is : "+ name);
+        // Concatenation
+        String firstName = "Tony";
+        String lastName = "Stark";
+        String fullName = firstName + "@" + lastName;
+        System.out.println(fullName.length());
+        // CharAt
+        for(int i=0; i<fullName.length(); i++) {
+            System.out.println(fullName.charAt(i));
+        }
+        // Compare
+        String name1 = "Tony";
+        String name2 = "Tony";
+        if(name1.compareTo(name2) == 0) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}
