a0f58ef82edc03067f15273521a4444b29ba36f5
MD PARVEJ <mparvejind@gmail.com>
2024-12-24 12:51:23 +0530

    upd

---
+++ dowhileloop.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class dowhileloop {
+    public static void main(String[] args) {
+        // do while loop
+        int i = 0;
+        do {
+            System.out.println(i);
+            i = i + 1;
+        } while (i < 5);
+    }
+}
