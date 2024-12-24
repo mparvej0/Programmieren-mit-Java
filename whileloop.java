605589d0d433e77dfef40be5a7d48ad7c7312da5
MD PARVEJ <mparvejind@gmail.com>
2024-12-24 12:58:13 +0530

    upd

---
+++ whileloop.java
@@ -0,0 +1,14 @@
+package Loops;
+
+public class whileloop {
+    public static void main(String[] args) {
+        // while loop
+        int i = 0;
+        while(i < 5) {
+            System.out.println(i);
+            i = i + 1;
+            // OR
+            // i++;
+        }
+    }
+}
