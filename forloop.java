74a038c885f44a9c535d0fd6a744185475f013a8
MD PARVEJ <mparvejind@gmail.com>
2024-12-24 12:54:36 +0530

    upd

--- forloop.java
+++ forloop.java
@@ -1,5 +1,4 @@
-package Loops;
-public class forloop {
+package Loops;public class forloop {
     public static void main(String[] args) {
         //for loop
         for(int counter = 0; counter < 5; counter = counter + 1) {
