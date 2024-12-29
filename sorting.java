85961592708ab02712799737756e4f37f0b58c5f
MD PARVEJ <mparvejind@gmail.com>
2024-12-29 18:52:20 +0530

    upd

--- sorting.java
+++ sorting.java
@@ -30,7 +30,19 @@ public class sorting {
             arr[i] = temp;
         }
 
+        // // Insertiion sort
+        // for(int i=1; i<arr.length; i++) {
+        //     int current = arr[i];
+        //     int j = i-1;
+        //     while(j >= 0 && arr[current] < arr[j]) {
+        //         arr[j+1] = arr[j];
+        //         j--;
+        //     }
+        //     // Placement
+        //     arr[j+1] = current;
+        // }
+
         printArray(arr);
-        
+
     }
 }
